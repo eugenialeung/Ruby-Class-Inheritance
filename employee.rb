11289ece9c50fd6ebb0431cf04211ee6edff68b6
eugenialeung <eleung316@gmail.com>
2019-08-21 12:25:38 -0700

    initial commit

---
+++ employee.rb
@@ -0,0 +1,54 @@
+class Employee
+
+    attr_reader :name, :title, :salary
+    attr_accessor :boss
+
+    def initialize(name, title, salary, boss = nil)
+        @name, @title, @salary = name, title, salary
+        self.boss = boss
+    end
+
+    def boss=(boss)
+        @boss = boss
+        boss.add_employee(self) unless boss.nil?
+        boss
+    end
+
+    def bonus(multiplier)
+        self.salary * multiplier
+    end
+
+end
+
+class Manager < Employee
+    attr_reader :employees
+
+    def initialize(name, title, salary, boss = nil)
+        @name, @title, @salary = name, title, salary
+        @employees = []
+    end
+
+    def add_employee(subordinate)
+        employees << subordinate
+        subordinate
+    end
+
+    def bonus(multiplier)
+        self.total_subsalary * multiplier
+    end
+
+    protected
+    
+    def total_subsalary
+        total_subsalary = 0
+        self.employees.each do |employee|
+          if employee.is_a?(Manager)
+            total_subsalary += employee.salary + employee.total_subsalary
+          else
+            total_subsalary += employee.salary
+          end
+        end
+    
+        total_subsalary
+    end
+end
